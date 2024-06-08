33fe1f7e57712ac4501fcb44429c1ebfa7d01e7f
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2024-06-08 02:33:10 +0200

    wip: new usage display

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,15 @@ fn read_stdin() -> String {
 }
 
 const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
-const HELP: &str = "Usage: osmia [--ctx [json-object]] [--help] [-v]";
+const HELP: &str =
+"--code          -- Add code as a JSON file                  
+--code-in       -- Add code as a JSON string from stdin     
+--code-str      -- Add code as a JSON string                
+--ctx           -- Add context as a JSON file               
+--ctx-in        -- Add context as a JSON string from stdin  
+--ctx-str       -- Add context as a JSON string             
+--help      -h  -- Display help information                 
+--version   -v  -- Display current version";
 
 fn main() {
 	let mut ctx: Option<String> = None;
